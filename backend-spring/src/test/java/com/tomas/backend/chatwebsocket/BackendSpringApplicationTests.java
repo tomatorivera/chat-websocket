96fe38174a3410933af049703eb9de58460d29ab
tomatorivera <tomatorivera@gmail.com>
2026-02-17 19:54:52 -0300

    crear aplicación de backend con spring

---
+++ backend-spring/src/test/java/com/tomas/backend/chatwebsocket/BackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomas.backend.chatwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
