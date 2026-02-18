e0b5dc3677f27412d63decf1cce29f391b6d6e87
tomatorivera <tomatorivera@gmail.com>
2026-02-17 21:06:45 -0300

    Implementar conexión y desconexión manual desde el frontend

--- frontend-angular/src/app/components/chat/chat.ts
+++ frontend-angular/src/app/components/chat/chat.ts
@@ -1,19 +1,19 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import * as Stomp from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-chat',
-  imports: [FormsModule, CommonModule],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './chat.html',
   styleUrl: './chat.css',
 })
 export class Chat {
 
   cliente!: Stomp.Client;
-  conectado: boolean = false;
+  conectado = signal<boolean>(false);
 
   ngOnInit(): void {
     // Configuro el cliente bajo el protocolo STOMP
@@ -31,16 +31,16 @@ export class Chat {
     // Acciones a realizar cuando el usuario se conecta
     // El frame es información sobre la conexión con el broker
     this.cliente.onConnect = (frame) => {
-      this.conectado = true;
+      this.conectado.set(true);
       console.log(`Conectado: ${this.cliente.connected} : ${frame}`);
     }
 
     this.cliente.onDisconnect = (frame) => {
-      this.conectado = false;
+      this.conectado.set(false);
       console.log(`Desconectado: ${!this.cliente.connected} : ${frame}`);
     }
 
-    this.conectarChat();
+    // this.conectarChat();
   }
 
   conectarChat(): void {
